f8033de149dc56b03f75d4e289e4ea1a167acd7f
Prakhar201 <75563800+Prakhar201@users.noreply.github.com>
2021-04-12 11:21:10 +0530

    Delete sprint_2 directory

--- sprint_2/server/middlewares/index.js
+++
@@ -1,8 +0,0 @@
-import expressJwt from "express-jwt";
-
-// req.user
-export const requireSignin = expressJwt({
-  // secret, expiryDate
-  secret: process.env.JWT_SECRET,
-  algorithms: ["HS256"],
-});
